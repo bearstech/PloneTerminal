8d3a2a210810a931c8d5a801ca0fb1f3900085a7
Gael Pasgrimaud <gael@gawel.org>
2011-04-30 01:06:38 +0200

    add completion + some commands

--- ploneterminal/sh.py
+++ ploneterminal/sh.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from plone.app.vocabularies.types import ReallyUserFriendlyTypesVocabularyFactory
+from plone.app.content.browser.foldercontents import FolderContentsTable
+from zope.component import getMultiAdapter
 from Products.Five import BrowserView
+import subprocess
 import logging
+import os
+
 try:
     import json
 except ImportError:
@@ -8,82 +14,51 @@ except ImportError:
 
 log = logging.getLogger(__name__)
 
+COMMANDS = {}
+for cmd in ('fortune', 'moo'):
+    try:
+        binary = subprocess.Popen(['which', cmd], stdout=subprocess.PIPE).stdout.read().strip()
+        if os.path.exists(binary):
+            COMMANDS[cmd] = binary
+            log.info('%s binary found at: %s', cmd, binary)
+        else:
+            log.error('No %s binary found in $PATH', cmd)
+    except Exception, e:
+        log.exception(e)
+
+
 class Shell(BrowserView):
 
-    def ls(self):
-        self.request.response.setHeader('Content-Type', 'text/plain')
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.portal = self.context.portal_url.getPortalObject()
+        self.portal_url = self.portal.absolute_url()
+        self.path = '/'.join(self.context.getPhysicalPath())
 
-        mtool = self.context.portal_membership
-        if mtool.isAnonymousUser():
-            member = member_id = is_admin = None
-        else:
-            member = self.context.portal_membership.getAuthenticatedMember()
-            member_id = str(member)
-            is_admin = 'Manager' in member.getRoles()
-
-        portal_url = self.context.portal_url.getPortalObject().absolute_url()
-        path = '/'.join(self.context.getPhysicalPath())
-        brains = self.context.portal_catalog.searchResults(
-                        path=dict(query=path, depth=1),
-                        object_provides=['Products.Archetypes.interfaces.base.IBaseContent'],
-                    )
-        results = {}
-        for b in brains:
-            url=b.getURL()
-            path = url[len(portal_url):]
-            results[path] = dict(
-                id=b.getId,
-                title=b.Title,
-                description=b.Description,
-                url=url,
-                path=path,
-                splitted_path=path.split('/'),
-                is_folderish=b.is_folderish,
-                modified=b.modified.strftime('%d %m %H:%M'),
-                size=b.getObjSize,
-                owner=b.Creator,
-            )
-        log.warn(results)
-        return json.dumps(results)
-
-
-    def info(self):
-        self.request.response.setHeader('Content-Type', 'text/plain')
-        portal_url = self.context.portal_url.getPortalObject().absolute_url()
-        path = '/'.join(self.context.getPhysicalPath())
-        brains = self.context.portal_catalog.searchResults(
-                        path=dict(query=path, depth=0),
-                        object_provides=['Products.Archetypes.interfaces.base.IBaseContent'],
-                    )
-        data = dict()
-        for b in brains:
-            if b.getPath() == path:
-                url=b.getURL()
-                path = url[len(portal_url):]
-                data = dict(
-                    id=b.getId,
-                    title=b.Title,
-                    description=b.Description,
-                    url=url,
-                    path=path,
-                    splitted_path=path.split('/'),
-                    is_folderish=b.is_folderish,
-                    modified=b.modified.strftime('%d %m %H:%M'),
-                    size=b.getObjSize,
-                    owner=b.Creator,
-                )
-        context = self.context
-        meth = getattr(self.context, 'CookedBody', None)
-        if meth is not None:
-            data['html_body'] = meth(stx_level=3)
-        meth = getattr(self.context, 'getText', None)
-        if meth is not None:
-            if 'html_body' not in data:
-                data['html_body'] = meth()
-            data['text_body'] = meth(mimetype='text/plain')
-        return json.dumps(data)
+    def brain_dict(self, b):
+        url=b.getURL()
+        path = url[len(self.portal_url):]
+        data = dict(
+            id=b.getId,
+            title_or_id=b.Title or b.getId,
+            description=b.Description,
+            url=url,
+            path=path,
+            splitted_path=path.split('/'),
+            folderish=b.is_folderish,
+            modified=b.modified.strftime('%d %m %H:%M'),
+            obj_type=b.Type,
+            size=b.getObjSize,
+            owner=b.Creator,
+        )
+        data.update(
+            sep='='*len(data['title_or_id']),
+          )
+        return data
 
     def server_info(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
         for k in ('HTTP_HOST', 'SERVER_NAME',):
             host = self.request.environ.get(k)
             if host:
@@ -92,12 +67,92 @@ class Shell(BrowserView):
         mtool = self.context.portal_membership
         if mtool.isAnonymousUser():
             member = 'guest'
+            ctypes = []
         else:
             member = str(mtool.getAuthenticatedMember())
+        ctypes = ReallyUserFriendlyTypesVocabularyFactory(self.context)
+        ctypes = [o.value for o in ctypes]
         data = dict(
-            member=member,
+            user=member,
             host=host,
             prompt='%s@%s:/$' % (member, host),
             prompt_mask='%s@%s:$cwd$' % (member, host),
+            create_vocabulary=ctypes,
           )
         return json.dumps(data)
+
+    def simple_cmd(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
+        cmd = self.request.form.get('cmd', '')
+        if cmd in COMMANDS:
+            data = subprocess.Popen([COMMANDS[cmd]], stdout=subprocess.PIPE).stdout.read().strip()
+            data = data.replace('\t', '    ')
+            data = data.split('\n')
+        else:
+            data = ['No %s found in $PATH' % cmd]
+        return json.dumps(dict(result=data))
+
+
+    def info(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
+        portal_url = self.context.portal_url.getPortalObject().absolute_url()
+        brains = self.context.portal_catalog.searchResults(path=self.path)
+        for b in brains:
+            if b.getPath() == self.path:
+                data = self.brain_dict(b)
+                break
+        context = self.context
+        meth = getattr(self.context, 'getText', None)
+        if meth is not None:
+            data['text_body'] = meth(mimetype='text/plain')
+        else:
+            data['text_body'] = ''
+
+        ctypes = ReallyUserFriendlyTypesVocabularyFactory(self.context)
+        ctypes = [o.value for o in ctypes]
+        data['ctypes'] = ctypes
+
+        log.warn(data)
+        return json.dumps(data)
+
+
+    def ls(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
+
+        ctypes = ReallyUserFriendlyTypesVocabularyFactory(self.context)
+        ctypes = [o.value for o in ctypes]
+
+        brains = self.context.portal_catalog.searchResults(
+                        path=dict(query=self.path, depth=1),
+                        Type=ctypes)
+
+        results = {}
+        for b in brains:
+            if b.getPath() == self.path:
+                continue
+            data = self.brain_dict(b)
+            path = data['path']
+            results[path] = data
+        log.warn(results)
+        return json.dumps(results)
+
+    def grep(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
+        portal_url = self.context.portal_url.getPortalObject().absolute_url()
+        results = {}
+        if 's' in self.request.form:
+            ctypes = ReallyUserFriendlyTypesVocabularyFactory(self.portal)
+            ctypes = [o.value for o in ctypes]
+            brains = self.context.portal_catalog.searchResults(
+                        SearchableText=self.request.form.get('s'),
+                        path=dict(query=self.path, depth=10),
+                        Type=ctypes, limit=100)
+            for b in brains:
+                if b.getPath() == self.path:
+                    continue
+                data = self.brain_dict(b)
+                path = data['path']
+                results[path] = data
+            log.warn(results)
+        return json.dumps(results)
+
--- ploneterminal/statics/ploneterminal.js
+++ ploneterminal/statics/ploneterminal.js
@@ -2,15 +2,21 @@ var Terminal = {};
 
 (function($){
 $.extend(Terminal, {
+    _term: null,
     _cwd: '/',
+    _cache: {},
     _options: {
                 height: '90%',
                 greetings:'',
                 prompt_mask: '$cwd$',
                 prompt: '/$'
              },
+    _error: function() { Terminal._term.error('An error occur') },
+    _no_such_directory: function() { Terminal._term.error('No such directory') },
+    _no_such_file_or_directory: function() { Terminal._term.error('No such file or directory') },
     _parse_command: function(line, term) {
         if (!line) return;
+        if (line[0] == '#') return;
         var args = line.split(' ');
         var meth = null;
         try {
@@ -20,7 +26,89 @@ $.extend(Terminal, {
             try {
                 Terminal[cmd](args, term);
             } catch (e) {term.error(e);};
-        } catch (e) {term.error(['No such command ', cmd, e]);};
+        } catch (e) {term.error(e+': No such command '+cmd);};
+    },
+    _complete_commands: function(cmd) {
+        for (k in Terminal) {
+            if (!/^_/.exec(k) && new RegExp('^'+cmd).exec(k))
+                return k + ' ';
+        }
+        return cmd;
+    },
+    _complete_vocabulary: function(value, vocab) {
+        var self = this;
+        if (!value) {
+            $(vocab).each(function(i, e) {self._term.echo(e);});
+        } else {
+            for (var i = 0; vocab.length; i++) {
+                k = vocab[i];
+                if (new RegExp('^'+value, 'i').exec(k))
+                    return k + ' ';
+            }
+        }
+        return value;
+    },
+    _complete_path: function(path) {
+        var self = this;
+        var match = self._path([path]);
+        match = match.replace(/\/\//, '/');
+        var path_info = match.split('/');
+        // self._term.error('path_info: '+path_info+' - match: ^'+match);
+        if (!/\/$/.exec(path)) {
+            path_info.pop();
+            if (path_info[0] != '') { path_info.splice(0, 0, '') }
+            if (path_info.length < 2 ) { path_info.splice(0, 0, '') }
+        }
+        resp = $.ajax({
+            type:'GET',
+            dataType:'json',
+            async: false,
+            url: $('#portal_url').attr('href')+path_info.join('/')+'/@@ls.sh'
+        });
+        if (!/^{/.exec(resp.responseText))
+           return path;
+        eval('var data = '+resp.responseText);
+        var possible = [];
+        for (k in data) {
+            if (new RegExp('^'+match, 'i').exec(k)) {
+                if (data[k].folderish)
+                    possible.push(k+'/');
+                else
+                    possible.push(k);
+            }
+        }
+        if (possible.length > 1 || /\/$/.exec(path)) {
+            $(possible).each(function() { self._term.echo(this); });
+        } else if (possible.length == 1) {
+            return possible[0];
+        } else {
+            $(possible).each(function() { self._term.echo(this); });
+        }
+        return path;
+    },
+    _complete: function(e) {
+        var self = Terminal;
+        if (e.which == 9 && !(e.ctrlKey || e.altKey)) {
+            var args = self._term.get_command();
+            args = args.split(' ');
+            if (args.length == 1) {
+                self._term.set_command(self._complete_commands(args[0]));
+            } else {
+                var cmd = args[0];
+                var vocab = self._options[cmd+'_vocabulary'];
+                if (vocab) {
+                    cmd = args.shift();
+                    var value = args.join(' ');
+                    value = self._complete_vocabulary(value, vocab);
+                    self._term.set_command(cmd + ' ' + value);
+                } else {
+                    var arg = self._complete_path(args[args.length-1]);
+                    args[args.length-1] = arg;
+                    self._term.set_command(args.join(' '));
+                }
+            }
+            return false;
+        }
     },
     _path: function(args) {
         var splited = this._cwd.split('/');
@@ -54,6 +142,18 @@ $.extend(Terminal, {
                 term.echo('- '+k);
         }
     },
+    pwd: function(args, term) {
+        term.echo(this._cwd);
+    },
+    quit: function(args, term) {
+        window.location.href = $('#portal_url').attr('href');
+    },
+    echo: function(args, term) {
+        args = args.join(' ');
+        args = args.replace(/^["\']/, '');
+        args = args.replace(/["\']$/, '');
+        term.echo(args);
+    },
     cd: function(args, term) {
         var self = this;
         path = self._path(args);
@@ -65,22 +165,18 @@ $.extend(Terminal, {
         $.ajax({
             type:'GET',
             dataType:'json',
+            async: false,
             url: $('#portal_url').attr('href')+path+'/@@info.sh',
             success: function(obj) {
-                if (!obj.is_folderish) {
+                if (!obj.folderish) {
                     term.error(path+' is not a directory');
                 } else {
                     self._cwd = path;
                     term.set_prompt(self._options['prompt_mask'].replace('$cwd', self._cwd));
                 }
             },
-            error: function() {
-                term.error('No such directory');
-            }
+            error: self._no_such_directory
         });
-    },
-    pwd: function(args, term) {
-        term.echo(this._cwd);
     },
     ls: function(args, term) {
         var self = this;
@@ -88,39 +184,194 @@ $.extend(Terminal, {
         $.ajax({
             type:'GET',
             dataType:'json',
+            async: false,
             url: $('#portal_url').attr('href')+path+'/@@ls.sh',
             success: function(data) {
                 for (k in data) {
                     var obj = data[k];
-                    if (obj.is_folderish)
-                        term.echo('[[b;#fff;transparent]'+obj.id+']/');
+                    if (obj.folderish)
+                        term.echo(obj.id+'[[b;#fff;transparent]/]');
                     else
-                        term.echo('[[;#fff;transparent]'+obj.id+']');
+                        term.echo(obj.id);
                 }
             },
-            error: function() {
-                term.error('No such directory');
-            }
+            error: self._no_such_directory
         });
     },
-    _open: function(obj, path, format, term) {
-        if (obj.is_folderish) {
-            term.error(path+' is a directory');
-            return;
+    grep: function(args, term) {
+        var self = this;
+        if (args.length == 2) {
+            var path = self._path([args.pop()]);
+            var s = args.pop();
+        } else {
+            term.echo('Usage: grep <term> <path>')
+            return true;
         }
-        $('.terminal-output').append('<h3 class="title">'+obj.title+'</h3>');
-        if (obj.description)
-            $('.terminal-output').append('<p class="description">'+obj.description+'</p>');
-        if (format == 'text' && obj.text_body) {
-            body = obj.text_body;
-            while (/\n\n\n/.exec(body))
-                body = body.replace('\n\n', '<br />');
-            $('.terminal-output').append('<p class="body">'+body+'</p>');
+        if (!s) {
+            term.echo('Usage: grep <term> <path>')
+            return true;
         }
-        if (format == 'html' && obj.html_body) {
-            $('.terminal-output').append('<p class="body">'+obj.html_body+'</p>');
+        $.ajax({
+            type:'POST',
+            dataType:'json',
+            data: {'s': s},
+            async: false,
+            url: $('#portal_url').attr('href')+path+'/@@grep.sh',
+            success: function(data) {
+                for (k in data) {
+                    var obj = data[k];
+                    if (obj.folderish)
+                        term.echo(obj.path+'[[b;#fff;transparent]/]');
+                    else
+                        term.echo(obj.path);
+                }
+            },
+            error: self._no_such_directory
+        });
+    },
+    create: function(args, term) {
+        var self = this;
+        $.ajax({
+            type:'GET',
+            dataType:'json',
+            async: false,
+            url: $('#portal_url').attr('href')+self._cwd+'/@@info.sh',
+            success: function(obj) {
+                if (!obj.folderish) {
+                    term.error(path+' is not a directory');
+                } else {
+                    window.open(obj.url+'/createObject?type_name='+args.join(' '));
+                }
+            },
+            error: self._no_such_directory
+        });
+    },
+    mkdir: function(args, term) {
+        self = this;
+        var initial_path = self._path(args);
+        var add_folder = function(path, id) {
+            $.ajax({
+                type:'GET',
+                dataType:'html',
+                async: false,
+                url: $('#portal_url').attr('href')+path+'/createObject?type_name=Folder',
+                success: function(html) {
+                    html = $(html);
+                    var form = $('form', $('#content', html));
+                    var data = {};
+                    $('input', form).each(function(i, e) {
+                        e = $(e);
+                        var k = e.attr('name');
+                        data[k] = e.val();
+                    });
+                    data['title'] = id;
+                    $.ajax({
+                        type:'POST',
+                        data: data,
+                        dataType:'html',
+                        async: false,
+                        url: form.attr('action'),
+                        success: function(resp) {
+                            if (/__ac_name/.exec(resp.responseText))
+                                term.error('Unauthorized')
+                        },
+                        error: self._error
+                    });
+                },
+                error: self._error
+            });
         }
-        term.echo('')
+        path = path.split('/')
+        var id = path.pop();
+        add_folder(path.join('/'), id);
+
+    },
+    rm: function(args, term) {
+        self = this;
+        if (args.length == 1) {
+            path = args.pop();
+            options = '-';
+        } else {
+            path = args.pop();
+            options = args.pop();
+        }
+        path = self._path([path]);
+        if (path == '/') {
+            term.error('WTF!!!????');
+            return true;
+        }
+        if (options[0] != '-' || !/^\//.exec(path)) {
+            term.error('Usage: rm [-Rf] path');
+            return true;
+        }
+
+        var delete_object = function(obj, html) {
+            if (!/f/.exec(options)) {
+                if (!confirm('Detete '+obj.path+' ?')) {
+                    return true;
+                }
+            }
+            var form = $('form', $('#content', html));
+            var data = {};
+            $('input', form).each(function(i, e) {
+                e = $(e);
+                var k = e.attr('name');
+                if (k != 'delete_all' && k != 'cancel')
+                    data[k] = e.val();
+            });
+            $.ajax({
+                type:'POST',
+                data: data,
+                dataType:'html',
+                async: false,
+                url: form.attr('action'),
+                success: function(resp) {
+                    if (/__ac_name/.exec(resp.responseText))
+                        term.error('Unauthorized')
+                },
+                error: self._error
+            });
+        }
+
+        $.ajax({
+            type:'GET',
+            dataType:'json',
+            async: false,
+            url: $('#portal_url').attr('href')+path+'/@@info.sh',
+            success: function(obj) {
+                if (obj.folderish) {
+                    term.error("You can't delete a directory");
+                    return;
+                }
+                $.ajax({
+                    type:'GET',
+                    dataType:'html',
+                    async: false,
+                    url: obj.url+'/delete_confirmation',
+                    success: function(html) {
+                        delete_object(obj, html);
+                    },
+                    error: function (resp) {
+                        delete_object(obj, $(resp.responseText));
+                    }
+                });
+            },
+            error: self._no_such_directory
+        });
+    },
+    file: function(args, term) {
+        var self = this;
+        path = self._path(args);
+        $.ajax({
+            type:'GET',
+            dataType:'json',
+            async:false,
+            url: $('#portal_url').attr('href')+path+'/@@info.sh',
+            success: function(obj) {
+                term.echo(obj.obj_type);
+            },
+            error: self._no_such_file_or_directory
+        });
     },
     cat: function(args, term) {
         var self = this;
@@ -128,30 +379,75 @@ $.extend(Terminal, {
         $.ajax({
             type:'GET',
             dataType:'json',
+            async:false,
             url: $('#portal_url').attr('href')+path+'/@@info.sh',
             success: function(obj) {
-                self._open(obj, path, 'text', term);
+                if (obj.folderish) {
+                    term.error(path+' is a directory');
+                    return;
+                }
+                term.echo(obj.title_or_id);
+                term.echo(obj.sep);
+                if (obj.description)
+                    term.echo(' ');
+                    term.echo(obj.description);
+                if (obj.text_body) {
+                    var body = obj.text_body;
+                    body = body.replace(/\n\s*\n/g, '\n');
+                    body = body.replace(/\s*$/g, ' ');
+                    body = body.replace(/^\s/, '');
+                    body = body.replace('\t', '    ');
+                    body = body.split('\n');
+                    term.echo(' ');
+                    term.echo(' ');
+                    $(body).each(function(i, e) {
+                       if (e) term.echo(e)
+                    });
+                }
+                term.echo('')
+                if (obj.obj_type == 'Image') {
+                    var img = new Image();
+                    $(img).load(function() {
+                        var div = $('.terminal-output', self._term).append('<div/>');
+                        div.css('width', '100%');
+                        div.append(img);
+                        term.echo(' ');
+                    })
+                    img.src = obj.url + '/image_thumb';
+                }
             },
-            error: function () {
-                term.error('No such file or directory');
-            }
+            error: self._no_such_file_or_directory
         });
     },
     open: function(args, term) {
         var self = this;
         path = self._path(args);
+        window.open($('#portal_url').attr('href')+path);
+    },
+    edit: function(args, term) {
+        var self = this;
+        path = self._path(args);
+        window.open($('#portal_url').attr('href')+path+'/edit');
+    },
+    _simple_cmd: function(cmd) {
+        var self = this;
         $.ajax({
-            type:'GET',
+            type:'POST',
             dataType:'json',
-            url: $('#portal_url').attr('href')+path+'/@@info.sh',
+            data: {'cmd': cmd},
+            url: $('#portal_url').attr('href')+'/@@simple_cmd.sh',
             success: function(obj) {
-                self._open(obj, path, 'html', term);
+                $(obj.result).each(function() { self._term.echo(this); });
             },
-            error: function () {
-                term.error('No such file or directory');
-            }
+            error: self._error
         });
     },
+    fortune: function(args, term) {
+        this._simple_cmd('fortune');
+    },
+    moo: function(args, term) {
+        this._simple_cmd('moo');
+    }
 });
 
 $(document).ready(function() {
@@ -160,8 +456,9 @@ $(document).ready(function() {
         dataType: 'json',
         url: $('#portal_url').attr('href')+'/server_info.sh',
         success: function(data) {
+            $.extend(Terminal._options, {'keydown':Terminal._complete});
             $.extend(Terminal._options, data);
-            $('#term').terminal(Terminal._parse_command, Terminal._options);
+            Terminal._term = $('#term').terminal(Terminal._parse_command, Terminal._options);
             $('.terminal-output').append($('#greetings').show());
         }
     });
